a69b7730bb5ac582ab04da806224e4a2ebdd7e14
Kartikeya Singh <kartikeya.official.ls@gmail.com>
2026-08-26 18:19:58 +0530

    Understanding_forloop

---
+++ Understanding_forloop.java
@@ -0,0 +1,7 @@
+public class Understanding_forloop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Hey, Kartik, this is the " + i + "'th iteration");
+        }
+    }
+}
