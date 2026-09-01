56d85de767e6cd634a53fbab28d1ca9612e5fc66
Kartikeya Singh <kartikeya.official.ls@gmail.com>
2026-09-01 23:53:31 +0530

    Pattern18

---
+++ Pattern18.java
@@ -0,0 +1,25 @@
+class Solution18 {
+    // Function to print Pattern 18
+    public void pattern18(int N) {
+        // Loop for each row
+        for (int i = 0; i < N; i++) {
+            // Print characters from ('A' + N - 1 - i) to ('A' + N - 1)
+            for (char ch = (char) ('A' + N - 1 - i); ch <= (char) ('A' + N - 1); ch++) {
+                System.out.print(ch + " ");
+            }
+            // Move to next line after each row
+            System.out.println();
+        }
+    }
+}
+
+public class Pattern18 {
+    public static void main(String[] args) {
+        // Create solution object
+        Solution18 sol = new Solution18();
+        // Define N
+        int N = 5;
+        // Call pattern function
+        sol.pattern18(N);
+    }
+}
