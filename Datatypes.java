8fe75f934370f31f4f34505eac298fe039405713
Kartikeya Singh <kartikeya.official.ls@gmail.com>
2026-09-07 10:09:22 +0530

    Datatypes

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Numric Datatypes-
+        int a = sc.nextInt();
+        byte b = sc.nextByte();
+        short s = sc.nextShort();
+        long l = sc.nextLong();
+        float f = sc.nextFloat();
+        double d = sc.nextDouble();
+        System.out.println(a);//??????
+
+    }
+
+}
