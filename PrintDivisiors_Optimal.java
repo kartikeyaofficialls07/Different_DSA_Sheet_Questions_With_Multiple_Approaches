a02f9ddf97c8c7bc07a0a3b1964d3f8bb796a57d
Kartikeya Singh <kartikeya.official.ls@gmail.com>
2026-09-10 08:57:38 +0530

    PrintDivisiors_Optimal

---
+++ PrintDivisiors_Optimal.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class SolutionPD {
+    // Function to get all divisors
+    public List<Integer> getDivisors(int N) {
+        // Create a list to store divisors
+        List<Integer> res = new ArrayList<>();
+
+        // Loop from 1 to square root of N
+        for (int i = 1; i * i <= N; i++) {
+            // Check if i divides N
+            if (N % i == 0) {
+                // Add i to result
+                res.add(i);
+
+                // If N / i is different from i, add N / i too
+                if (i != N / i) {
+                    res.add(N / i);
+                }
+            }
+        }
+
+        // Return the list of divisors
+        return res;
+    }
+}
+
+public class PrintDivisiors_Optimal {
+    public static void main(String[] args) {
+        // Create object of Solution class
+        SolutionPD sol = new SolutionPD();
+
+        // Input number
+        int N = 36;
+
+        // Get divisors
+        List<Integer> result = sol.getDivisors(N);
+
+        // Print the result
+        System.out.print("Divisors of " + N + ": ");
+        for (int val : result) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+}
